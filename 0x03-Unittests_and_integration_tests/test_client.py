63f9a23dc235ab0b47b4e1732bfa88f2f98f7320
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-07-01 07:16:09 -0700

    9. Integration tests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -4,6 +4,7 @@ import unittest
 from unittest.mock import Mock, patch, PropertyMock
 from client import GithubOrgClient
 from parameterized import parameterized
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -65,3 +66,49 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient('org')
         res = client.has_license(repo, license)
         self.assertEqual(res, expected)
+
+
+@parameterized_class([{
+    'org_payload': TEST_PAYLOAD[0][0],
+    'repos_payload': TEST_PAYLOAD[0][1],
+    'expected_repos': TEST_PAYLOAD[0][2],
+    'apache2_repos': TEST_PAYLOAD[0][3]
+    }])
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """ integration test class for GithubOrgClient """
+    @classmethod
+    def setUpClass(cls):
+        """ setUp method """
+        payload = {
+                'https://api.github.com/orgs/google': cls.org_payload,
+                'https://api.github.com/orgs/google/repos': cls.repos_payload,
+                }
+        def get_payload(url):
+            if url in route_payload:
+                return Mock(**{'json.return_value': route_payload[url]})
+            return HTTPError
+
+        cls.get_patcher = patch("requests.get", side_effect=get_payload)
+        cls.get_patcher.start()
+
+    def test_public_repos(self) -> None:
+        """_summary_
+        """
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(),
+            self.expected_repos,
+        )
+
+    def test_public_repos_with_license(self) -> None:
+        """_summary_
+        """
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(license="apache-2.0"),
+            self.apache2_repos,
+        )
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """_summary_
+        """
+        cls.get_patcher.stop()
